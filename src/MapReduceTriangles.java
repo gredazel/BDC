ddd40938b29ffbd0f7e05ba7fb34c2b81a36285c
michelezadro <zadro.michele@gmail.com>
2023-03-24 17:02:42 +0100

    Count Triangles

--- src/MapReduceTriangles.java
+++ src/MapReduceTriangles.java
@@ -3,12 +3,50 @@ import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import org.apache.spark.storage.StorageLevel;
+import scala.Tuple2;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Random;
 public class MapReduceTriangles {
+
+        public static Long CountTriangles(ArrayList<Tuple2<Integer, Integer>> edgeSet) {
+                if (edgeSet.size()<3) return 0L;
+                HashMap<Integer, HashMap<Integer,Boolean>> adjacencyLists = new HashMap<>();
+                for (int i = 0; i < edgeSet.size(); i++) {
+                        Tuple2<Integer,Integer> edge = edgeSet.get(i);
+                        int u = edge._1();
+                        int v = edge._2();
+                        HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+                        HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+                        if (uAdj == null) {uAdj = new HashMap<>();}
+                        uAdj.put(v,true);
+                        adjacencyLists.put(u,uAdj);
+                        if (vAdj == null) {vAdj = new HashMap<>();}
+                        vAdj.put(u,true);
+                        adjacencyLists.put(v,vAdj);
+                }
+                Long numTriangles = 0L;
+                for (int u : adjacencyLists.keySet()) {
+                        HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+                        for (int v : uAdj.keySet()) {
+                                if (v>u) {
+                                        HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+                                        for (int w : vAdj.keySet()) {
+                                                if (w>v && (uAdj.get(w)!=null)) numTriangles++;
+                                        }
+                                }
+                        }
+                }
+                return numTriangles;
+        }
+
         static final int p = 8191; // constant used to calculate hash function
         public int MR_ApproxTCwithNodeColors(int c, JavaRDD<String> edges) {
                 return 0;
         }
+
+
 
         /**
          * Calculate the value of the hash function of a given vertex u
@@ -16,7 +54,6 @@ public class MapReduceTriangles {
          * @param u value of the considered vertex
          * @return hash function's value of vertex u
          */
-        /*CIAO*/
         private int hashFunct(int c, Integer u){
                 Random rand = new Random();
                 int a = rand.nextInt(p);
