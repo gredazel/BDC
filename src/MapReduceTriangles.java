5ee3e5ccd55af4981116564460fbebdf0c0b181b
Greta Piai <greta.piai@studenti.unipd.it>
2023-03-24 11:30:07 +0100

    Inserting HW1 directory and creating method hashFunct to calculate hash function's value for a given vertex u

---
+++ src/MapReduceTriangles.java
@@ -0,0 +1,25 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.storage.StorageLevel;
+import java.util.Random;
+public class MapReduceTriangles {
+        static final int p = 8191; // constant used to calculate hash function
+        public int MR_ApproxTCwithNodeColors(int c, JavaRDD<String> edges) {
+                return 0;
+        }
+
+        /**
+         * Calculate the value of the hash function of a given vertex u
+         * @param c integer parameter used to partition data
+         * @param u value of the considered vertex
+         * @return hash function's value of vertex u
+         */
+        private int hashFunct(int c, int u){
+                Random rand = new Random();
+                int a = rand.nextInt(p);
+                int b = rand.nextInt(p);
+                return (((a*u)+b)%p)%c;
+        }
+}
