7f31ed5bc80ebead96ab044ed4fdf5249610a80b
TheSav1101 <emanuele.f.savoia@gmail.com>
2023-03-27 15:49:19 +0200

    fine alg1

--- src/G088HW1.java
+++ src/G088HW1.java
@@ -1,11 +1,15 @@
+import javassist.bytecode.Descriptor;
 import org.apache.spark.SparkConf;
 import org.apache.spark.api.java.JavaPairRDD;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
 import scala.Tuple2;
+import spire.macros.Auto;
 
+import javax.swing.text.html.HTMLDocument;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.Random;
 
 public class G088HW1 {
@@ -21,8 +25,8 @@ public class G088HW1 {
         JavaSparkContext sc = new JavaSparkContext(conf);
         sc.setLogLevel("WARN");
         JavaRDD<String> docs = sc.textFile(filepath).repartition(8).cache();
-
-        System.out.println("Number of triangles =" + MR_ApproxTCwithNodeColors(C, docs));
+        for (int i = 0; i < R; i++)
+            System.out.println("Number of triangles =" + MR_ApproxTCwithNodeColors(C, docs));
     }
 
     static final int p = 8191; // constant used to calculate hash function
@@ -57,7 +61,9 @@ public class G088HW1 {
         return numTriangles;
     }
     public static long MR_ApproxTCwithNodeColors(int c, JavaRDD<String> edges) {
-
+        Random rand = new Random();
+        int a = rand.nextInt(p - 1) + 1;
+        int b = rand.nextInt(p);
         JavaPairRDD<Integer, Tuple2<Integer, Integer>> coloredEdges;
         coloredEdges = edges.flatMapToPair((document) -> {
             String[] tokens = document.split("\\r?\\n");
@@ -67,11 +73,11 @@ public class G088HW1 {
                 String verteces[] = token.split(",");
                 int vert1 = Integer.parseInt(verteces[0]);
                 int vert2 = Integer.parseInt(verteces[1]);
-                Integer a = hashFunct(c, vert1);
-                Integer b = hashFunct(c, vert2);
+                int color1 = hashFunct(c, vert1, a, b);
+                int color2 = hashFunct(c, vert2, a, b);
                 Tuple2<Integer, Integer> val = new Tuple2<>(vert1, vert2);
-                if (a == b){
-                    edgesSets.add(new Tuple2<>(a, val));
+                if (color1 == color2){
+                    edgesSets.add(new Tuple2<>(color1, val));
                 }
             } // THIS is FOR CREATING THE C PARTITIONS AND PUT IN THEM ASSOCIATED EDGES
             return edgesSets.iterator();
@@ -82,10 +88,14 @@ public class G088HW1 {
             for(Tuple2<Integer, Integer> elem : e._2()){
                 E.add(elem);
             }
-            return new Tuple2<>(0, CountTriangles(E));
+            long tri = CountTriangles(E);
+            System.out.println(tri);
+            return new Tuple2<>(0, tri);
         });
 
-        long totTriangles = c*c * counted.reduce((x, y) -> new Tuple2<>(0, x._2() + y._2()))._2();
+
+
+        long totTriangles = c*c * counted.reduceByKey((x,y) -> x + y).first()._2();
         return totTriangles;
     }
 
@@ -95,10 +105,9 @@ public class G088HW1 {
      * @param u value of the considered vertex
      * @return hash function's value of vertex u
      */
-    private static int hashFunct(int c, Integer u){
-        Random rand = new Random();
-        int a = rand.nextInt(p);
-        int b = rand.nextInt(p);
+
+
+    private static int hashFunct(int c, Integer u, int a, int b){
         return (((a*u)+b)%p)%c;
     }
 }
