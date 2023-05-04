8e80522e1dee6c3582405c4944f9940530aad1fe
Greta Piai <greta.piai@studenti.unipd.it>
2023-05-04 15:39:12 +0200

    Import new class for second homework in src (G088HW2.java; inside there is already all stuff we already done in the previous and is working)

---
+++ src/G088HW2.java
@@ -0,0 +1,188 @@
+import org.apache.spark.SparkConf;
+import org.apache.spark.api.java.JavaPairRDD;
+import org.apache.spark.api.java.JavaRDD;
+import org.apache.spark.api.java.JavaSparkContext;
+import scala.Tuple2;
+import scala.Tuple3;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Random;
+
+public class G088HW2 {
+    static final int p = 8191; // constant used to calculate hash function
+
+    public static void main(String[] args){
+        if(args.length < 3){
+            System.out.println("TOO FEW ARGS");
+        }
+        int C = Integer.parseInt(args[0]);
+        int R = Integer.parseInt(args[1]);
+        String filepath = args[2];
+        SparkConf conf = new SparkConf(true).setAppName("Triangles");
+        JavaSparkContext sc = new JavaSparkContext(conf);
+        sc.setLogLevel("WARN");
+
+        //convert string RDD into JavaPairRDD<Integer, Integer>
+        JavaPairRDD<Integer, Integer> edges = MakeEdgeRDD(sc.textFile(filepath)).repartition(16).cache();
+
+        System.out.println("Dataset = " + filepath);
+        System.out.println("Number of edges = " + edges.count());
+        System.out.println("Number of colors = " + C);
+        System.out.println("Number of repetitions = " + R);
+
+        //fill array with results of R runs and calculate average time of node colouring
+        ArrayList<Long> ColorApprox = new ArrayList<>();
+        long avgTime = System.currentTimeMillis();
+        for (int i = 0; i < R; i++){
+            ColorApprox.add(MR_ApproxTCwithNodeColors(C, edges));
+        }
+        avgTime = System.currentTimeMillis() - avgTime;
+        Collections.sort(ColorApprox);
+        avgTime /= R;
+
+
+        System.out.println("Approximation through node coloring");
+        System.out.println("- Number of triangles (median over " + R + " runs) = " + ColorApprox.get(R/2));
+        System.out.println("- Running time (average over " + R + " runs) = " + avgTime + "ms");
+
+    }
+
+    public static Long CountTriangles(ArrayList<Tuple2<Integer, Integer>> edgeSet) {
+        if (edgeSet.size()<3) return 0L;
+        HashMap<Integer, HashMap<Integer,Boolean>> adjacencyLists = new HashMap<>();
+        for (int i = 0; i < edgeSet.size(); i++) {
+            Tuple2<Integer,Integer> edge = edgeSet.get(i);
+            int u = edge._1();
+            int v = edge._2();
+            HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+            HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+            if (uAdj == null) {uAdj = new HashMap<>();}
+            uAdj.put(v,true);
+            adjacencyLists.put(u,uAdj);
+            if (vAdj == null) {vAdj = new HashMap<>();}
+            vAdj.put(u,true);
+            adjacencyLists.put(v,vAdj);
+        }
+        Long numTriangles = 0L;
+        for (int u : adjacencyLists.keySet()) {
+            HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+            for (int v : uAdj.keySet()) {
+                if (v>u) {
+                    HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+                    for (int w : vAdj.keySet()) {
+                        if (w>v && (uAdj.get(w)!=null)) numTriangles++;
+                    }
+                }
+            }
+        }
+        return numTriangles;
+    }
+
+    public static Long CountTriangles2(ArrayList<Tuple2<Integer, Integer>> edgeSet, Tuple3<Integer, Integer, Integer> key, long a, long b, long p, int C) {
+        if (edgeSet.size()<3) return 0L;
+        HashMap<Integer, HashMap<Integer,Boolean>> adjacencyLists = new HashMap<>();
+        HashMap<Integer, Integer> vertexColors = new HashMap<>();
+        for (int i = 0; i < edgeSet.size(); i++) {
+            Tuple2<Integer,Integer> edge = edgeSet.get(i);
+            int u = edge._1();
+            int v = edge._2();
+            if (vertexColors.get(u) == null) {vertexColors.put(u, (int) ((a*u+b)%p)%C);}
+            if (vertexColors.get(v) == null) {vertexColors.put(v, (int) ((a*v+b)%p)%C);}
+            HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+            HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+            if (uAdj == null) {uAdj = new HashMap<>();}
+            uAdj.put(v,true);
+            adjacencyLists.put(u,uAdj);
+            if (vAdj == null) {vAdj = new HashMap<>();}
+            vAdj.put(u,true);
+            adjacencyLists.put(v,vAdj);
+        }
+        Long numTriangles = 0L;
+        for (int u : adjacencyLists.keySet()) {
+            HashMap<Integer,Boolean> uAdj = adjacencyLists.get(u);
+            for (int v : uAdj.keySet()) {
+                if (v>u) {
+                    HashMap<Integer,Boolean> vAdj = adjacencyLists.get(v);
+                    for (int w : vAdj.keySet()) {
+                        if (w>v && (uAdj.get(w)!=null)) {
+                            ArrayList<Integer> tcol = new ArrayList<>();
+                            tcol.add(vertexColors.get(u));
+                            tcol.add(vertexColors.get(v));
+                            tcol.add(vertexColors.get(w));
+                            Collections.sort(tcol);
+                            boolean condition = (tcol.get(0).equals(key._1())) && (tcol.get(1).equals(key._2())) && (tcol.get(2).equals(key._3()));
+                            if (condition) {numTriangles++;}
+                        }
+                    }
+                }
+            }
+        }
+        return numTriangles;
+    }
+
+    /**
+     * ALGORITHM 1
+     * @param c number of partitions
+     * @param edges JavaPairRDD containing all edges
+     * @return approximation of triangles number using color approximation
+     */
+    public static long MR_ApproxTCwithNodeColors(int c, JavaPairRDD<Integer, Integer> edges) {
+        Random rand = new Random();
+        int a = rand.nextInt(p - 1) + 1;
+        int b = rand.nextInt(p);
+
+        return edges.flatMapToPair((token) -> {
+
+            ArrayList<Tuple2<Integer, Tuple2<Integer, Integer>>> edgesSets = new ArrayList<>();
+            int color1 = hashFunct(c, token._1(), a, b);
+            int color2 = hashFunct(c, token._2(), a, b);;
+            if (color1 == color2){
+                edgesSets.add(new Tuple2<>(color1, token));
+            }
+            return edgesSets.iterator();
+        }).groupByKey().mapToPair((e) ->{
+            ArrayList<Tuple2<Integer, Integer>> E = new ArrayList<>();
+            for(Tuple2<Integer, Integer> elem : e._2()){
+                E.add(elem);
+            }
+            return new Tuple2<>(0, CountTriangles(E));
+        }).reduceByKey((x,y) -> x + y).first()._2() *c *c;
+
+    }
+
+    public int MR_ExactTC(JavaPairRDD<Integer, Integer> edge, int c){
+        int exactCount = 0;
+        //
+        // TO IMPLEMENT !!!!!
+        //
+        return exactCount;
+    }
+
+    /**
+     * Method to convert into JavaPairRDD a JavaRDD (in this case the edges RDD)
+     * @param stringEdges input RDD of strings
+     * @param stringEdges input RDD
+     * @return conversion to JavaPairRDD<Integer, Integer>
+     */
+    public static JavaPairRDD<Integer, Integer> MakeEdgeRDD(JavaRDD<String> stringEdges){
+        return stringEdges.mapToPair((token) -> {
+            String verteces[] = token.split(",");
+            int vert1 = Integer.parseInt(verteces[0]);
+            int vert2 = Integer.parseInt(verteces[1]);
+            return new Tuple2<>(vert1, vert2);});
+    }
+
+    /**
+     * Calculate the value of the hash function of a given vertex u
+     * @param c integer parameter used to partition data
+     * @param u value of the considered vertex
+     * @param a random integer in [1, p-1] fixed for every run
+     * @param b random integer in [0, p-1] fixed for every run
+     * @return hash function's value of vertex u
+     */
+    private static int hashFunct(int c, Integer u, int a, int b){
+        return (((a*u)+b)%p)%c;
+    }
+}
